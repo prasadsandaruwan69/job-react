78ce85b6b76e8a904362724a2b958b13208612f5
prasad sandaruwan <prasadsandaruwan069@gmail.com>
2025-02-21 18:22:52 +0530

    Update Users.js

--- src/Users.js
+++ src/Users.js
@@ -1,25 +1,35 @@
 import { Box } from "@mui/material";
 import Userform from "./Userform";
 import UserTable from "./UserTable";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
-const users =[
+const Users = () => {
+  const [users, setUsers] = useState([]);
 
-    {
-        id:1,
-        name:"prasad",
-    },
-    {
-        id:2,
-        name:'prasd',
-    }
-]
-const  Users = () =>{
-return(
-    <Box sx={{width:'calc(100% - 100px)',margin:'auto' , marginTop:'100px'}}>
-          <Userform />
-          <UserTable rows={users} />
-    </Box>
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = () => {
+    Axios.get("http://127.0.0.1:3001/api/users") // ✅ Fix: Added "http://"
+      .then((response) => {
+        console.log(response.data);
+        setUsers(response.data.response); // ✅ Ensure users are stored in state
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  };
   
-);
-}
+
+  return (
+    <Box
+      sx={{ width: "calc(100% - 100px)", margin: "auto", marginTop: "100px" }}
+    >
+      <Userform />
+      <UserTable rows={users} />
+    </Box>
+  );
+};
 export default Users;
